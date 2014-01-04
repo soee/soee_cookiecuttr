2f5d595e511189f08c1045fcc2cb5d5dc34b94ee
Marcin Sągol <kontakt@soee.pl>
2014-01-04 01:44:32 +0100

    [BUGFIX] Fix plugin flexform configuration file name

--- ext_tables.php
+++ ext_tables.php
@@ -11,7 +11,7 @@ if (!defined('TYPO3_MODE')) {
 
 $pluginSignature = str_replace('_', '', $_EXTKEY) . '_cookiecuttr';
 $TCA['tt_content']['types']['list']['subtypes_addlist'][$pluginSignature] = 'pi_flexform';
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForm/CookiecuttrmPlugin.xml');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue($pluginSignature, 'FILE:EXT:' . $_EXTKEY . '/Configuration/FlexForm/CookiecuttrPlugin.xml');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
     'mod.wizards.newContentElement.wizardItems.soee {
